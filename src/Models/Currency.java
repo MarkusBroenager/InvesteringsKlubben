1773a93e5fa64b8833c8887c218ea9fff2fdc4fc
Frede-72 <frjo0005@stud.kea.dk>
2025-04-29 17:44:15 +0200

    ADDED: currency class

---
+++ src/Models/Currency.java
@@ -0,0 +1,36 @@
+package Models;
+
+import java.time.LocalDate;
+
+public class Currency {
+
+    private String baseCurrency;
+    private String quoteCurrency;
+    private double rate;
+    private LocalDate lastUpdated;
+
+    public Currency(String baseCurrency, String quoteCurrency, double rate, LocalDate lastUpdated) {
+        this.baseCurrency = baseCurrency;
+        this.quoteCurrency = quoteCurrency;
+        this.rate = rate;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public String toString() {
+        if(String.valueOf(rate).contains("0.0")){
+            return "1 " + baseCurrency + " is currently worth " + rate +
+                    " " + quoteCurrency + " Last update: " + lastUpdated;
+        }
+        return "1 " + baseCurrency + " is currently worth " + String.format("%.2f",rate) +
+                " " + quoteCurrency + " Last update: " + lastUpdated;
+    }
+
+}
