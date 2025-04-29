12a2b4c95de4e150beefd998d48271b8a8b86fdf
Frede-72 <frjo0005@stud.kea.dk>
2025-04-29 16:55:56 +0200

    ADDED: stock class

---
+++ src/Models/Stock.java
@@ -0,0 +1,36 @@
+package Models;
+
+import java.time.LocalDate;
+
+public class Stock {
+
+    private String ticker;
+    private String name;
+    private String sector;
+    private double price;
+    private String currency;
+    private String rating;
+    private double dividend;
+    private String market;
+    private LocalDate lastUpdated;
+
+
+    public Stock(String ticker, String name, String sector,double price,String currency,
+                 String rating,double dividend,String market,LocalDate lastUpdated) {
+        this.name = name;
+        this.ticker = ticker;
+        this.sector = sector;
+        this.price = price;
+        this.currency = currency;
+        this.rating = rating;
+        this.dividend = dividend;
+        this.market = market;
+        this.lastUpdated = lastUpdated;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + ticker + ") Sector: " + sector + " Price: " + price + " " +
+                currency + " Dividend: " + dividend +"% Exchange: " + market + " Rating: " + rating + " Last updated: " + lastUpdated;
+    }
+}
