c3fac293d2a0e0998035fb559b454f93a30454a3
Frede-72 <frjo0005@stud.kea.dk>
2025-04-29 11:49:10 +0200

    ADDED: User class

---
+++ src/Models/User.java
@@ -0,0 +1,35 @@
+package Models;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private int userID;
+    private String fullName;
+    private String email;
+    private double initialCash;
+    private LocalDate birthday;
+    private LocalDate createdAt;
+    private LocalDate lastUpdated;
+
+    public User(int userID, String fullName, String email, LocalDate birthday, double initialCash, LocalDate createdAt, LocalDate lastUpdated) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.email = email;
+        this.initialCash = initialCash;
+        this.birthday = birthday;
+        this.createdAt = createdAt;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String addToCSVFile() {
+        return userID + ";" + fullName + ";" + email + ";" + birthday +
+                ";" + initialCash + ";" + createdAt + ";" + lastUpdated;
+    }
+
+    public String toString() {
+        return "Name: " + fullName + " userID: " + userID + " Born: " + birthday + " email: " + email +
+                " Started investing: " + createdAt + " Initial investment in dkk: " + initialCash +" Last trade:" + lastUpdated;
+    }
+
+}
