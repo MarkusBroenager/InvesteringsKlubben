d9f39bd4e9c4b5f4bf81a9e5f3c071f121a85780
Frede-72 <frjo0005@stud.kea.dk>
2025-04-30 11:07:11 +0200

    ADDED: controller

---
+++ src/Controller.java
@@ -0,0 +1,30 @@
+import Models.Transaction;
+import Services.StockMarketService;
+import Services.TransactionService;
+import Services.UserService;
+
+import java.util.List;
+
+public class Controller {
+
+    private StockMarketService stockMarketService;
+    private TransactionService transactionService;
+    private UserService userService;
+
+    public Controller(StockMarketService stockMarketService, TransactionService transactionService,
+                      UserService userService) {
+        this.stockMarketService = stockMarketService;
+        this.transactionService = transactionService;
+        this.userService = userService;
+    }
+
+    public void start(){
+        int userID = 1;
+        List<Transaction> transactions = transactionService.getTransactionsForUser(userID);
+        System.out.println("Showings transactions for " + userService.getUser(userID));
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+    }
+
+}
