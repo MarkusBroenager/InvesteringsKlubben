66f279d5ab9f539790b9b8ed49f7e17e50980e74
Frede-72 <frjo0005@stud.kea.dk>
2025-04-30 10:44:32 +0200

    static methods used for parsing

--- src/Services/DataServices.java
+++ src/Services/DataServices.java
@@ -2,13 +2,17 @@ package Services;
 
 import java.time.LocalDate;
 
-public class LocalDateCreator {
+public class DataServices {
 
-    public static LocalDate create(String date){
+    public static LocalDate getLocalDate(String date){
         String[] dateSplit = date.split("-");
         if(Integer.parseInt(dateSplit[2])>1900){
             return LocalDate.of(Integer.parseInt(dateSplit[2]),Integer.parseInt(dateSplit[1]),Integer.parseInt(dateSplit[0]));
         }
         return LocalDate.parse(date);
+    }
+
+    public static double stringToDouble(String getDouble) {
+        return Double.parseDouble(getDouble.replace(",", "."));
     }
 }
